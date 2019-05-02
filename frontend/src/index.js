b05b5fe7084a48265dabb1ffbfb71703d57f8989
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-05-02 18:04:55 +0300

    Add frontend app with React scaffolding
    * Move backend API endpoints from / to /api/

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('Hello, world!');
+ReactDOM.render(<App />, document.getElementById('app'));
--- prediction_market/urls.py
+++ prediction_market/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.urls')),
+    path('', include('frontend.urls')),
+    path('api/', include('backend.urls')),
 ]
