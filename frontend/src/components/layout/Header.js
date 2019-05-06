9da0b9ad2df1d66f60bd88304c17a43831daa7d8
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-05-06 11:20:45 +0300

    Use bootstrap in Header component

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -13,21 +13,32 @@ class Header extends Component {
   render() {
     const { user, isAuthenticated } = this.props.auth;
     const authLinks = (
-      <ul>
-        <li><button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">Logout</button></li>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">Logout</button>
+        </li>
       </ul>
     );
     const guestLinks = (
-        <ul>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-        </ul>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">Login</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">Register</Link>
+        </li>
+      </ul>
     );
     return (
-      <header className="container">
-        <h1><Link to="/">Prediction market</Link></h1>
-        {isAuthenticated ? authLinks : guestLinks}
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">Prediction market</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          {isAuthenticated ? authLinks : guestLinks}
+        </div>
+      </nav>
     );
   }
 }
