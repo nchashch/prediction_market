f3d151375d388694c3033c977300598f6b369c24
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-05-16 00:19:12 +0300

    Rewrite Market component as a function using hooks

--- frontend/src/components/predictionMarket/Market.js
+++ frontend/src/components/predictionMarket/Market.js
@@ -1,32 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { getMarkets } from '../../actions/markets';
-import PropTypes from 'prop-types';
 import Outcomes from '../predictionMarket/Outcomes';
 
-class Market extends Component {
-  static propTypes = {
-    marketId: PropTypes.string.isRequired,
-    markets: PropTypes.array.isRequired,
-    getMarkets: PropTypes.func.isRequired
-  }
-
-  componentDidMount() {
-    this.props.getMarkets();
-  }
-
-  render() {
-    return (
-        <main>
-          <Outcomes marketId={this.props.marketId} />
-        </main>
-    );
-  }
+function Market(props) {
+  const marketId = props.match.params.marketId
+  return (
+    <main>
+      <Outcomes marketId={marketId} />
+    </main>
+  );
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  markets: state.markets.markets,
-  marketId: ownProps.match.params.marketId
-});
-
-export default connect(mapStateToProps, { getMarkets })(Market);
+export default Market;
