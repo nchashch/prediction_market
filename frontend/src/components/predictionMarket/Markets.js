5fd6bc71ca34f40f0b2860ab9ff0b1f2d5408f11
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-05-15 23:21:24 +0300

    Rewrite Markets component as a function component using hooks

--- frontend/src/components/predictionMarket/Markets.js
+++ frontend/src/components/predictionMarket/Markets.js
@@ -1,61 +1,49 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux'
 import { getMarkets, deleteMarket } from '../../actions/markets';
 
-export class Markets extends Component {
-  static propTypes = {
-    markets: PropTypes.array.isRequired,
-    getMarkets: PropTypes.func.isRequired,
-    deleteMarket: PropTypes.func.isRequired
-  }
-
-  componentDidMount() {
-    this.props.getMarkets();
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <h1>Market list</h1>
-        <ul>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>B</th>
-                <th>Number of outcomes</th>
-                <th>Start date</th>
-                <th>End date</th>
-                <th>Resolved</th>
-                <th></th>
+function Markets() {
+  const markets = useSelector(state => state.markets.markets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMarkets());
+  });
+  return (
+    <Fragment>
+      <h1>Market list</h1>
+      <ul>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>B</th>
+              <th>Number of outcomes</th>
+              <th>Start date</th>
+              <th>End date</th>
+              <th>Resolved</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            { markets.map((market) => (
+              <tr key={market.id}>
+                <td>{market.id}</td>
+                <td><Link to={'/markets/' + market.id}>{market.name}</Link></td>
+                <td>{market.b}</td>
+                <td>{market.number_of_outcomes}</td>
+                <td>{market.start_date}</td>
+                <td>{market.end_date}</td>
+                <td>{market.resolved ? 'true' : 'false'}</td>
+                <td><button onClick={() => { dispatch(deleteMarket(market.id)) } } className="btn btn-danger btn-sm">Delete</button></td>
               </tr>
-            </thead>
-            <tbody>
-              { this.props.markets.map((market) => (
-                <tr key={market.id}>
-                  <td>{market.id}</td>
-                  <td><Link to={'/markets/' + market.id}>{market.name}</Link></td>
-                  <td>{market.b}</td>
-                  <td>{market.number_of_outcomes}</td>
-                  <td>{market.start_date}</td>
-                  <td>{market.end_date}</td>
-                  <td>{market.resolved ? 'true' : 'false'}</td>
-                  <td><button onClick={this.props.deleteMarket.bind(this, market.id)} className="btn btn-danger btn-sm">Delete</button></td>
-                </tr>
-              )) }
-            </tbody>
-          </table>
-        </ul>
-      </Fragment>
-    );
-  }
+            )) }
+          </tbody>
+        </table>
+      </ul>
+    </Fragment>
+  );
 }
 
-const mapStateToProps = state => ({
-  markets: state.markets.markets
-});
-
-export default connect(mapStateToProps, { getMarkets, deleteMarket })(Markets)
+export default Markets;
