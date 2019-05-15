1bf2d6f012a50857b44f6f16b428511bbd8a65f5
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-05-16 00:12:19 +0300

    Rewrite Positions component as a function using hooks

--- frontend/src/components/predictionMarket/Positions.js
+++ frontend/src/components/predictionMarket/Positions.js
@@ -1,55 +1,46 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getPositions } from '../../actions/positions';
 
-export class Positions extends Component {
-  static propTypes = {
-    positions: PropTypes.array.isRequired,
-    getPositions: PropTypes.func.isRequired
-  }
+function Positions() {
+  const positions = useSelector(state => state.positions.positions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPositions());
+  }, []);
 
-  componentDidMount() {
-    this.props.getPositions();
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <h1>Position list</h1>
-        <ul>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Market</th>
-                <th>Outcome</th>
-                <th>Portfolio</th>
-                <th>Amount</th>
-                <th>Closed</th>
+  return (
+    <Fragment>
+      <h1>Position list</h1>
+      <ul>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Market</th>
+              <th>Outcome</th>
+              <th>Portfolio</th>
+              <th>Amount</th>
+              <th>Closed</th>
+            </tr>
+          </thead>
+          <tbody>
+            { positions.map((position) => (
+              <tr key={position.id}>
+                <td>{position.id}</td>
+                <td>{position.market}</td>
+                <td>{position.outcome}</td>
+                <td>{position.portfolio}</td>
+                <td>{position.amount}</td>
+                <td>{position.closed ? 'true' : 'false'}</td>
               </tr>
-            </thead>
-            <tbody>
-              { this.props.positions.map((position) => (
-                <tr key={position.id}>
-                  <td>{position.id}</td>
-                  <td>{position.market}</td>
-                  <td>{position.outcome}</td>
-                  <td>{position.portfolio}</td>
-                  <td>{position.amount}</td>
-                  <td>{position.closed ? 'true' : 'false'}</td>
-                </tr>
-              )) }
-            </tbody>
-          </table>
-        </ul>
-      </Fragment>
-    );
-  }
+            )) }
+          </tbody>
+        </table>
+      </ul>
+    </Fragment>
+  );
 }
 
-const mapStateToProps = state => ({
-  positions: state.positions.positions
-});
-
-export default connect(mapStateToProps, { getPositions })(Positions);
+export default Positions;
