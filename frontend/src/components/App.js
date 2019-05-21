bc568ae14d9a4fea8dfe344ada48b347bae83df3
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-05-21 22:24:21 +0300

    Rewrite App component as a function using hooks

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -12,7 +12,6 @@ import store from '../store';
 import Alerts from './layout/Alerts';
 import Login from './accounts/Login';
 import Register from './accounts/Register';
-import PrivateRoute from './common/PrivateRoute';
 import { loadUser } from '../actions/auth';
 
 // Alert Options
@@ -21,35 +20,33 @@ const alertOptions = {
   position: 'top center'
 }
 
-class App extends Component {
-  componentDidMount() {
+function App() {
+  useEffect(() => {
     store.dispatch(loadUser());
-  }
+  }, []);
 
-  render() {
-    return (
-      <Provider store={store}>
-        <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Router>
-            <Fragment>
-              <Header />
-              <div className="container">
-                <Switch>
-                  <Route exact path="/" component={Markets} />
-                  <Route exact path="/register" component={Register} />
-                  <Route exact path="/login" component={Login} />
-                  <Route path="/markets/:marketId" component={Market} />
-                  <Route path="/markets/" component={Markets} />
-                  <Route path="/positions/" component={Positions} />
-                  <Route path="/orders/" component={Orders} />
-                </Switch>
-              </div>
-            </Fragment>
-          </Router>
-        </AlertProvider>
-      </Provider>
-    );
-  }
+  return (
+    <Provider store={store}>
+      <AlertProvider template={AlertTemplate} {...alertOptions}>
+        <Router>
+          <Fragment>
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Markets} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+                <Route path="/markets/:marketId" component={Market} />
+                <Route path="/markets/" component={Markets} />
+                <Route path="/positions/" component={Positions} />
+                <Route path="/orders/" component={Orders} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </AlertProvider>
+    </Provider>
+  );
 }
 
 export default App;
